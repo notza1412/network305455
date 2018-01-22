b9a01035a02d08d3b20cd1af99b956adf6ff5306
notza1412 <voranathp58@email.nu.ac.th>
2018-01-23 05:31:51 +0700

    add file TCP2

---
+++ Homework1/TCP/TCP2/Check.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5008
+TEMP = 'UploadPicture/'
+FileName = 'JOJO.jpg'
+
+print "Picture:",TEMP+FileName
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+UploadFile = open(TEMP+FileName,"rb")
+Read = UploadFile.read(1024)
+
+while Read:
+    sock.send(Read)
+    Read = UploadFile.read(1024)
+
+print "Done"
